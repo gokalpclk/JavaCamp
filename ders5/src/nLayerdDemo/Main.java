bb230d148554069e580ae28026b300e2150d9ecf
gokalpclk <06gokalp@gmail.com>
2021-05-20 03:59:15 +0300

    lecture5

---
+++ ders5/src/nLayerdDemo/Main.java
@@ -0,0 +1,18 @@
+package nLayerdDemo;
+
+import nLayerdDemo.business.abstracts.ProductService;
+import nLayerdDemo.business.concretes.ProductManager;
+import nLayerdDemo.core.JLoggerManagerAdapter;
+import nLayerdDemo.dataAccess.concretes.AbcProductDao;
+import nLayerdDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ProductService productService = new ProductManager(new AbcProductDao(), new JLoggerManagerAdapter());
+		Product product = new Product(1, 2, "Elma", 12, 50);
+		productService.add(product);
+
+	}
+
+}
---
+++ ders5/src/nLayerdDemo/business/concretes/ProductManager.java
@@ -0,0 +1,39 @@
+package nLayerdDemo.business.concretes;
+
+import java.util.List;
+
+import nLayerdDemo.business.abstracts.ProductService;
+import nLayerdDemo.core.LoggerService;
+import nLayerdDemo.dataAccess.abstracts.ProductDao;
+import nLayerdDemo.entities.concretes.Product;
+
+public class ProductManager implements ProductService {
+
+	private ProductDao productDao;
+	private LoggerService loggerService;
+
+	public ProductManager(ProductDao productDao, LoggerService loggerService) {
+		super();
+		this.productDao = productDao;
+		this.loggerService = loggerService;
+	}
+
+	@Override
+	public void add(Product product) {
+		if (product.getCategoryId() == 1) {
+			System.out.println("Bu kategoride urun kabul edilmiyor");
+			return;
+		}
+
+		this.productDao.add(product);
+		this.loggerService.logToSystem("Urun eklnedi: " + product.getName());
+
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
